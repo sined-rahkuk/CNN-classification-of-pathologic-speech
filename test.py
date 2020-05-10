f52286cf2cb0db233d63a053b24d9295857c0347
Kristian <kristilutak83@gmail.com>
2020-05-10 13:43:58 +0200

    first test of machine training

--- test.py
+++ test.py
@@ -11,8 +11,7 @@ from keras.models import Sequential
 from keras.optimizers import SGD
 from keras.preprocessing.image import ImageDataGenerator
 
-PATH_TO_DATA = "img_data"
-
+PATH_TO_DATA = "wav_data"
 
 # Python 3.6.8
 # Keras	2.3.1
@@ -45,51 +44,48 @@ PATH_TO_DATA = "img_data"
 # six	1.14.0
 # split-folders	0.3.1
 # TensorFlow 2.1.0
-# TODO: Короче треба найти якусь базу аудіо (там де будуть і ковдуші і нормальні люди без патологій) шоб аналізірувати
 
-# Іспользуємо цю хуйню шоб конвертірувати аудіо файл у .png шоб потом його аналізірувати
-# Можна убрати def convert_to_png, я то добавив просто для читабельності
-
-# Тут буде лиш два типа - хуй з патологією, хуй без патології
+# Іспользуємо цю хуйню шоб конвертірувати аудіо файл у .png шоб потом його аналізірувати (converts WAV to png)
 for g in ['healthy', 'pathological']:
-	pathlib.Path(f'img_data/{g}').mkdir(parents=True, exist_ok=True)  # Создасть папку img_data
-	for filename in os.listdir(f'./{PATH_TO_DATA}/{g}'):
-		if filename and filename.find('.png') != -1:
-			continue
-		songname = f'./{PATH_TO_DATA}/{g}/{filename}'
-		y, sr = librosa.load(songname, mono=True, duration=10)
-		print(y.shape)
-		plt.specgram(y, NFFT=2048, Fs=2, Fc=0, noverlap=128, sides='default', mode='default',
-		             scale='dB')
-		plt.axis('off')
-		plt.savefig(f'img_data/{g}/{filename[:-3].replace(".", "")}.png')
-		plt.clf()
-print('proshel for')
-# 80 процетів хуйні для треніровки і 20 процентів хуйні для тестів
-split_folders.ratio('./img_data/', output="./img_data", seed=1337, ratio=(.8, .2))
+    print('Creating png from wav')
+    pathlib.Path(f'img_data/{g}').mkdir(parents=True, exist_ok=True)  # Создасть папку img_data
+    for filename in os.listdir(f'./{PATH_TO_DATA}/{g}'):
+        if filename and filename.find('.png') != -1:
+            continue
+        songname = f'./{PATH_TO_DATA}/{g}/{filename}'
+        y, sr = librosa.load(songname, mono=True, duration=10)
+        plt.specgram(y, NFFT=2048, Fs=2, Fc=0, noverlap=128, sides='default', mode='default',
+                     scale='dB')
+        plt.axis('off')
+        plt.savefig(f'img_data/{g}/{filename[:-3].replace(".", "")}.png')
+        plt.clf()
+
+# 80 процетів хуйні для треніровки і 20 процентів хуйні для тестів (test(20) and train(80) data)
+print('Split all data to train and test')
+split_folders.ratio('./img_data/', output="./data", seed=1337, ratio=(.8, .2))
 
 # Керасовська залупа яка рандомно мутірує пнгшку і получаються ліпші дата для треніровки
 train_datagen = ImageDataGenerator(
-	rescale=1. / 255,  # rescale all pixel values from 0-255, so after this step all our pixel values are in range (0,1)
-	shear_range=0.2,  # to apply some random transformations
-	zoom_range=0.2,  # to apply zoom
-	horizontal_flip=True)  # image will be flipper horiz
+    rescale=1. / 255,  # rescale all pixel values from 0-255, so after this step all our pixel values are in range (0,1)
+    shear_range=0.2,  # to apply some random transformations
+    zoom_range=0.2,  # to apply zoom
+    horizontal_flip=True)  # image will be flipper horiz
 test_datagen = ImageDataGenerator(rescale=1. / 255)
 
 training_set = train_datagen.flow_from_directory(
-	'./img_data/train',
-	target_size=(64, 64),
-	batch_size=32,
-	class_mode='categorical',
-	shuffle=False)
+    './data/train',
+    target_size=(64, 64),
+    batch_size=32,
+    class_mode='categorical',
+    shuffle=False)
 test_set = test_datagen.flow_from_directory(
-	'./img_data/val',
-	target_size=(64, 64),
-	batch_size=32,
-	class_mode='categorical',
-	shuffle=False)
+    './data/val',
+    target_size=(64, 64),
+    batch_size=32,
+    class_mode='categorical',
+    shuffle=False)
 
-# CNN модель, не знаю шо значать ці леєри но цю хуйню нам нада
+# CNN модель, не знаю шо значать ці леєри но цю хуйню нам нада (CNN model)
 model = Sequential()
 input_shape = (64, 64, 3)
 # 1st hidden layer
@@ -112,29 +108,29 @@ model.add(Dense(64))
 model.add(Activation('relu'))
 model.add(Dropout(rate=0.5))
 # Output layer
-model.add(Dense(10))
+model.add(Dense(2))
 model.add(Activation('softmax'))
 model.summary()
 
-epochs = 200
-batch_size = 8
-learning_rate = 0.01
-decay_rate = learning_rate / epochs
-momentum = 0.9  # Хз шо це, но похоже на заклінаніє із гарі потера
-
-# Stochastic Gradient Descent це для треніровки, но тут можна і другу модель іспользувати
-sgd = SGD(lr=learning_rate, momentum=momentum, decay=decay_rate, nesterov=False)
+# epochs = 200
+# batch_size = 8
+# learning_rate = 0.01
+# decay_rate = learning_rate / epochs
+# momentum = 0.9  # Хз шо це, но похоже на заклінаніє із гарі потера
+#
+# # Stochastic Gradient Descent це для треніровки, но тут можна і другу модель іспользувати
+# sgd = SGD(lr=learning_rate, momentum=momentum, decay=decay_rate, nesterov=False)
 model.compile(optimizer="sgd", loss="categorical_crossentropy", metrics=['accuracy'])
 
-# Запускаємо модель
+# Запускаємо модель (start model)
 model.fit_generator(
-	training_set,
-	steps_per_epoch=100,
-	epochs=50,
-	validation_data=test_set,
-	validation_steps=200)
+    training_set,
+    steps_per_epoch=50,
+    epochs=5,
+    validation_data=test_set,
+    validation_steps=200)
 
-# Це вже лиш результати
+# Це вже лиш результати (results)
 model.evaluate_generator(generator=test_set, steps=50)
 
 test_set.reset()
@@ -145,11 +141,8 @@ predicted_class_indices = np.argmax(pred, axis=1)
 labels = training_set.class_indices
 labels = dict((v, k) for k, v in labels.items())
 predictions = [labels[k] for k in predicted_class_indices]
-predictions = predictions[:200]
 filenames = test_set.filenames
+predictions = predictions[:len(filenames)]
 
-print(len(filenames), len(predictions))
-
-results = pd.DataFrame({"Filename": filenames,
-                        "Predictions": predictions})
+results = pd.DataFrame({"Filename": filenames, "Predictions": predictions})
 results.to_csv("prediction_results.csv", index=False)
